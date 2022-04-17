134d98af8026e33bec4cad10e56f173e8d90c882
robert <robert456@gmail.com>
2022-04-17 18:56:46 +0300

    add new chart remove old chart up version

--- simpleapp.py
+++ simpleapp.py
@@ -13,5 +13,5 @@ def _post_():
     data = request.get_data()
     return data
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.run(debug=True, host='0.0.0.0', port=port)
